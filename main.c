5a7fdc435513ec61899a1055b9ac0de54c3ac0da
Itamar2562 <zzitamar66@gmail.com>
2026-07-06 18:59:22 +0300

    added start for serverside logic after learning a bit bout it

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #define _POSIX_C_SOURCE 200112L
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
+
+#define MAXCLIENTS 2
+#define SERVERPORT "4555"
 
 
 void printAddressIPV4(struct sockaddr_in* addr)
@@ -48,32 +54,94 @@ void printAddresses(struct addrinfo* addresses){
 }
 
 
-int main()
+int GetServerSocket()
 {
-
-  struct addrinfo hints;
-  struct addrinfo *res; //results
+  struct addrinfo hints, *res;
+  int sockfd=-1;
 
   memset(&hints,0,sizeof hints); //clear the struct from garabge
-  hints.ai_family= AF_UNSPEC; 
+  hints.ai_family= AF_INET; 
   hints.ai_socktype =SOCK_STREAM; //tcp
   hints.ai_flags=AI_PASSIVE;
 
-  char *port = "6380"; //can be number or name :0
-
-  int status=getaddrinfo(NULL,port,&hints, &res);
-  if (status!=0){
-
+  int status=getaddrinfo(NULL,SERVERPORT,&hints, &res);
+  if (status!=0)
+  {
     printf("error is %s\n", gai_strerror(status));
-    return 1;
+    return -1;
   }
-  int sockfd= socket(res->ai_family, res->ai_socktype , res->ai_protocol);
 
-  bind (sockfd, res->ai_addr, res->ai_addrlen);
+  for (struct addrinfo * p=res; p!=NULL; p=p->ai_next )
+  {
+    if (p->ai_family!=hints.ai_family || p->ai_socktype != hints.ai_socktype)
+      continue;
+    
+      sockfd=socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+      if (sockfd ==-1)
+      {
+        perror("server socket error: ");
+        continue;
+      }
 
-  printf("im on test");
- 
+      int bindStatus=bind(sockfd, p->ai_addr,p->ai_addrlen);
+      if (bindStatus==-1){
+        perror("server bind error");
+        close(sockfd);
+        continue;
+      }
+
+      break; //finished all steps
+  }
   freeaddrinfo(res);
+  return sockfd;
+}
 
-    return 0;
+void * getCorrectSinAddress(struct sockaddr* genericAddr)
+{
+  if (genericAddr->sa_family==AF_INET){
+    return &((struct sockaddr_in* )genericAddr)->sin_addr;
+  }
+
+  return &((struct sockaddr_in6* )genericAddr)->sin6_addr;
+}
+
+
+
+
+int main()
+{
+  int sockfd=GetServerSocket();
+  if (sockfd==-1){
+    exit(1);
+  }
+  
+  int Listenstatus= listen(sockfd, MAXCLIENTS);
+  if (Listenstatus==-1){
+    perror("server listen error");
+    exit(1);
+  }
+ 
+   printf("listening for incoming clients..\n");
+  struct sockaddr_storage ClientAddr;
+  socklen_t ClientAddrLen=sizeof(ClientAddr);
+
+  while (1){
+      int ClientSockfd = accept(sockfd, (struct sockaddr* )&ClientAddr, &ClientAddrLen);
+      if (ClientSockfd==-1)
+      {
+        perror("Connect error");
+        continue;
+      }
+      char buffer[INET6_ADDRSTRLEN];
+
+      void* sin_addr=getCorrectSinAddress((struct sockaddr* )&ClientAddr);
+      inet_ntop(ClientAddr.ss_family,sin_addr ,buffer,INET6_ADDRSTRLEN);
+
+      printf("got connection from %s\n",buffer);
+      
+  }
+
+
+
+  return 0;
 }
